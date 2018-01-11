3ad41ee844155c418c976b1cae30fde60033a8d4
johtela <tommi.johtela@dnainternet.net>
2018-01-11 20:44:27 +0200

    Tweaked scripts.

--- Scripts/NuGet.ps1
+++ Scripts/NuGet.ps1
@@ -3,19 +3,24 @@ $project = "ExtensionCord"
 $version = "1.0.0"
 $projectdir = "$nugetdir\$project"
 
-function BuildPackage ()
+function PackDebug ()
 {
 	& $nugetdir\nuget.exe pack $project.csproj
 }
 
-function ReleaseLocal ()
+function PackRelease ()
 {
-	BuildPackage 
+	& $nugetdir\nuget.exe pack $project.csproj -properties Configuration=Release
+}
+
+function DeployLocal ()
+{
+	PackDebug
 	if (Test-Path $projectdir)
 	{
 		Remove-Item $projectdir -Recurse
 	}
-	& $nugetdir\nuget.exe add "$project.$version.nupkg" -source $nuget
+	& $nugetdir\nuget.exe add "$project.$version.nupkg" -source $nugetdir
 }
 
-ReleaseLocal
+DeployLocal
